192d48bb86d8fc0ce2c9aa9396d6dcc48ceb613a
diamondburned <diamond@arikawa-hi.me>
2022-11-20 20:49:36 -0800

    Fix aocutil test

--- aocutil/aocutil_test.go
+++ aocutil/aocutil_test.go
@@ -5,12 +5,12 @@ import (
 	"testing"
 )
 
-func TestWindow(t *testing.T) {
+func TestSlidingWindow(t *testing.T) {
 	var num = []int{0, 1, 2, 3, 4}
 	var res = [][]int{{0, 1}, {1, 2}, {2, 3}, {3, 4}}
 	var turn int
 
-	Window(num, 2, func(w []int) {
+	SlidingWindow(num, 2, func(w []int) {
 		if !reflect.DeepEqual(res[turn], w) {
 			t.Errorf("mismatch at %d: %v", turn, w)
 		}

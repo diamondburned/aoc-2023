39dfda1ca9dc67b0c84bf34996506d99bcee0dda
diamondburned <diamond@arikawa-hi.me>
2022-12-02 22:02:33 -0800

    Add more functions into aocutil

--- aocutil/aocutil.go
+++ aocutil/aocutil.go
@@ -1,12 +1,19 @@
 package aocutil
 
 import (
+	"bufio"
+	"bytes"
+	"container/heap"
+	"io"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 
 	"golang.org/x/exp/constraints"
+
+	_ "gonum.org/v1/gonum"
 )
 
 // ReadFile reads a file into a string, panicking if it fails.
@@ -57,7 +64,7 @@ func SlidingWindow[T any](slice []T, size int, fn func([]T)) {
 // Chunk splits a slice into chunks of size n, calling fn for each chunk.
 func Chunk[T any](numbers []T, size int, fn func([]T)) {
 	for i := 0; i < len(numbers); i += size {
-		end := Min(i+size, len(numbers))
+		end := Min2(i+size, len(numbers))
 		fn(numbers[i:end])
 	}
 }
@@ -97,8 +104,8 @@ func MinMaxes[T constraints.Ordered](numbers []T) (min, max T) {
 	return
 }
 
-// Max returns the maximum of two numbers.
-func Max[T constraints.Ordered](vs ...T) T {
+// Maxs returns the maximum of many values.
+func Maxs[T constraints.Ordered](vs ...T) T {
 	var max T
 	for _, v := range vs {
 		if v > max {
@@ -108,8 +115,16 @@ func Max[T constraints.Ordered](vs ...T) T {
 	return max
 }
 
-// Min returns the minimum of two numbers.
-func Min[T constraints.Ordered](vs ...T) T {
+// Max2 returns the maximum of two values.
+func Max2[T constraints.Ordered](a, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// Mins returns the minimum of many values.
+func Mins[T constraints.Ordered](vs ...T) T {
 	var min T
 	for _, v := range vs {
 		if v < min {
@@ -117,6 +132,14 @@ func Min[T constraints.Ordered](vs ...T) T {
 		}
 	}
 	return min
+}
+
+// Min2 returns the minimum of two values.
+func Min2[T constraints.Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 // Clamp returns the value clamped to the range [min, max].
@@ -167,4 +190,203 @@ func Assertf(cond bool, f string, v ...any) {
 	if !cond {
 		log.Panicf(f, v...)
 	}
+}
+
+// SplitN splits a string into n parts, and panics if the number of parts is not
+// n.
+func SplitN(s, sep string, n int) []string {
+	parts := strings.SplitN(s, sep, n)
+	Assertf(len(parts) == n, "SplitN(%q, %q, %d): got %d", s, sep, n, len(parts))
+	return parts
+}
+
+// FieldsN splits a string into n fields, and panics if the number of fields is
+// not n.
+func FieldsN(s string, n int) []string {
+	parts := strings.Fields(s)
+	Assertf(len(parts) == n, "FieldsN(%q, %d): got %d", s, n, len(parts))
+	return parts
+}
+
+// Heap is a heap of ordered values.
+type Heap[T constraints.Ordered] internalHeap[T]
+
+type HeapOpts[T constraints.Ordered] struct {
+	// Less is a custom less function. If nil, min or max will be used. This
+	// takes precedence over Max.
+	Less func(a, b T) bool
+	// Max is true if the heap is a max heap. Largest values will be put first.
+	// Otherwise, the heap is a min heap.
+	Max bool
+	// Cap is the preallocated capacity of the heap.
+	Cap int
+	// Limit determines the behavior of Push when the heap is full. A zero-value
+	// sets no limit.
+	Limit int
+}
+
+func (h HeapOpts[T]) less(a, b T) bool {
+	if h.Less != nil {
+		return h.Less(a, b)
+	}
+	if h.Max {
+		return a > b
+	}
+	return a < b
+}
+
+func (h HeapOpts[T]) more(a, b T) bool {
+	if h.Less != nil {
+		// https://pkg.go.dev/sort#Interface
+		if h.Less(a, b) {
+			// a < b
+			return false
+		}
+		if h.Less(b, a) {
+			// !(a < b) && b < a
+			// = a >= b && b < a
+			// = a > b
+			return true
+		}
+		// !(a < b) && !(b < a)
+		// = a >= b && b >= a
+		// = a == b
+		return false
+	}
+	if h.Max {
+		return a < b
+	}
+	return a > b
+}
+
+// NewMinHeap returns a new min heap.
+func NewMinHeap[T constraints.Ordered]() *Heap[T] {
+	return NewHeapOpts(HeapOpts[T]{Max: false})
+}
+
+// NewMaxHeap returns a new max heap.
+func NewMaxHeap[T constraints.Ordered]() *Heap[T] {
+	return NewHeapOpts(HeapOpts[T]{Max: true})
+}
+
+// NewHeapOpts creates a new heap with options.
+func NewHeapOpts[T constraints.Ordered](opts HeapOpts[T]) *Heap[T] {
+	slice := make([]T, 0, opts.Cap)
+	h := internalHeap[T]{
+		heap: slice,
+		opts: opts,
+	}
+	heap.Init((*internalHeap[T])(&h))
+	return (*Heap[T])(&h)
+}
+
+// Push pushes a value onto the heap.
+func (h *Heap[T]) Push(v T) {
+	if h.opts.Limit == 0 || len(h.heap) < h.opts.Limit {
+		heap.Push((*internalHeap[T])(h), v)
+		return
+	}
+
+	min := h.heap[0]
+	if h.opts.more(v, min) {
+		x := h.Pop()
+		heap.Push((*internalHeap[T])(h), v)
+		log.Println("heap limit exceeded, popped", x, "for", v)
+	}
+}
+
+// Pop pops a value from the heap.
+func (h *Heap[T]) Pop() T {
+	return heap.Pop((*internalHeap[T])(h)).(T)
+}
+
+// Fix fixes the heap after a value has been modified at the given index.
+func (h *Heap[T]) Fix(i int) {
+	heap.Fix((*internalHeap[T])(h), i)
+}
+
+// Len returns the number of items in the heap.
+func (h *Heap[T]) Len() int {
+	return len(h.heap)
+}
+
+// Sort sorts the heap.
+func (h *Heap[T]) Sort() {
+	sort.Sort((*internalHeap[T])(h))
+}
+
+// ToSlice returns the underlying heap slice.
+func (h *Heap[T]) ToSlice() []T {
+	return h.heap
+}
+
+// ToSortedSlice returns a sorted slice of the heap.
+func (h *Heap[T]) ToSortedSlice() []T {
+	h.Sort()
+	return h.heap
+}
+
+type internalHeap[T constraints.Ordered] struct {
+	heap []T
+	opts HeapOpts[T]
+}
+
+var _ heap.Interface = (*internalHeap[int])(nil)
+var _ sort.Interface = (*internalHeap[int])(nil)
+
+func (h internalHeap[T]) Len() int {
+	return len(h.heap)
+}
+
+func (h internalHeap[T]) Less(i, j int) bool {
+	return h.opts.less(h.heap[i], h.heap[j])
+}
+
+func (h internalHeap[T]) Swap(i, j int) {
+	h.heap[i], h.heap[j] = h.heap[j], h.heap[i]
+}
+
+// Push pushes the element x onto the heap.
+func (h *internalHeap[T]) Push(x interface{}) {
+	h.heap = append(h.heap, x.(T))
+}
+
+// Pop removes the minimum element (according to Less) from the heap and returns
+// it.
+func (h *internalHeap[T]) Pop() interface{} {
+	old := h.heap
+	n := len(old)
+	x := old[n-1]
+	h.heap = old[0 : n-1]
+	return x
+}
+
+// Scanner is a scanner for a reader.
+type Scanner struct {
+	s *bufio.Scanner
+}
+
+// NewScanner returns a new scanner for a reader.
+func NewScanner(r io.Reader) *Scanner {
+	return &Scanner{s: bufio.NewScanner(r)}
+}
+
+// NewBytesScanner returns a new scanner for a byte slice.
+func NewBytesScanner(b []byte) *Scanner {
+	return NewScanner(bytes.NewReader(b))
+}
+
+// SetSplitter sets the split function for the underlying scanner.
+func (s *Scanner) SetSplitter(scanner bufio.SplitFunc) {
+	s.s.Split(scanner)
+}
+
+// Next returns the next token.
+func (s *Scanner) Next() bool {
+	return s.s.Scan()
+}
+
+// Token returns the current token.
+func (s *Scanner) Token() string {
+	return s.s.Text()
 }
--- aocutil/aocutil_test.go
+++ aocutil/aocutil_test.go
@@ -30,3 +30,80 @@ func TestChunk(t *testing.T) {
 		turn++
 	})
 }
+
+func TestHeap(t *testing.T) {
+	type test struct {
+		name string
+		opts HeapOpts[int]
+		in   []int
+		out  []int
+	}
+
+	tests := []test{
+		{
+			name: "min_preorder",
+			opts: HeapOpts[int]{},
+			in:   []int{1, 2, 3, 4, 5},
+			out:  []int{1, 2, 3, 4, 5},
+		},
+		{
+			name: "min_reverse",
+			opts: HeapOpts[int]{},
+			in:   []int{5, 4, 3, 2, 1},
+			out:  []int{1, 2, 3, 4, 5},
+		},
+		{
+			name: "min_random",
+			opts: HeapOpts[int]{},
+			in:   []int{3, 5, 1, 4, 2},
+			out:  []int{1, 2, 3, 4, 5},
+		},
+		{
+			name: "max_preorder",
+			opts: HeapOpts[int]{Max: true},
+			in:   []int{5, 4, 3, 2, 1},
+			out:  []int{5, 4, 3, 2, 1},
+		},
+		{
+			name: "max_reverse",
+			opts: HeapOpts[int]{Max: true},
+			in:   []int{1, 2, 3, 4, 5},
+			out:  []int{5, 4, 3, 2, 1},
+		},
+		{
+			name: "max_random",
+			opts: HeapOpts[int]{Max: true},
+			in:   []int{3, 5, 1, 4, 2},
+			out:  []int{5, 4, 3, 2, 1},
+		},
+		{
+			name: "min_limit",
+			opts: HeapOpts[int]{Limit: 3},
+			in:   []int{3, 5, 1, 4, 2},
+			out:  []int{1, 2, 3},
+		},
+		{
+			name: "max_limit",
+			opts: HeapOpts[int]{Max: true, Limit: 3},
+			in:   []int{3, 5, 1, 4, 2},
+			out:  []int{5, 4, 3},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			h := NewHeapOpts(test.opts)
+			for _, v := range test.in {
+				h.Push(v)
+			}
+
+			t.Logf("heap: %v", h.ToSlice())
+
+			for _, v := range test.out {
+				if h.Pop() != v {
+					t.Errorf("mismatch: %v", h)
+				}
+			}
+		})
+	}
+}

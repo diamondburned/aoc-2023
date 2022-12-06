c8ad6f7173baa4d21a614f95092aa3cc89c81235
diamondburned <diamond@arikawa-hi.me>
2022-12-05 22:50:38 -0800

    Clean up 06

--- 06/main.go
+++ 06/main.go
@@ -1,55 +1,19 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 
 	"github.com/diamondburned/aoc-2022/aocutil"
 )
 
 func main() {
-	input := []byte(aocutil.Input())
+	input := aocutil.InputBytes()
 
 	const packetStartLen = 4
-	packetStartIx := packetStartLen
-	var packetStart []byte
-
-	aocutil.SlidingWindow(input, packetStartLen, func(window []byte) bool {
-		// Ensure that the bytes in this window are all unique.
-		if !isUniq(window) {
-			packetStartIx++
-			return false
-		}
-
-		packetStart = window
-		return true
-	})
-
-	fmt.Println(packetStartIx, string(packetStart))
+	packetStartIx := aocutil.SlidingWindow(input, packetStartLen, aocutil.IsUniq[byte])
+	fmt.Println(packetStartIx+packetStartLen, string(input[packetStartIx:packetStartIx+packetStartLen]))
 
 	const messageStartLen = 14
-	messageStartIx := messageStartLen
-	var messageStart []byte
-
-	aocutil.SlidingWindow(input, messageStartLen, func(window []byte) bool {
-		// Ensure that the bytes in this window are all unique.
-		if !isUniq(window) {
-			messageStartIx++
-			return false
-		}
-
-		messageStart = window
-		return true
-	})
-
-	fmt.Println(messageStartIx, string(messageStart))
-}
-
-func isUniq(b []byte) bool {
-	for _, c := range b {
-		if bytes.Count(b, []byte{c}) > 1 {
-			return false
-		}
-	}
-	return true
+	msgStartIx := aocutil.SlidingWindow(input, messageStartLen, aocutil.IsUniq[byte])
+	fmt.Println(msgStartIx+messageStartLen, string(input[msgStartIx:msgStartIx+messageStartLen]))
 }

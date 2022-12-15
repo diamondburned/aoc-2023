a85c8470524a4365be084fc096e86a619e83ac56
diamondburned <diamond@arikawa-hi.me>
2022-12-15 04:35:02 -0800

    Clean up 15 and hyperoptimize it

--- 15/main.go
+++ 15/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
-	"log"
+	"image"
 	"math"
 	"runtime"
 	"sort"
-	"time"
+	"sync"
+	"sync/atomic"
 
 	"github.com/diamondburned/aoc-2022/aocutil"
 )
@@ -30,120 +30,100 @@ func main() {
 		}
 	}
 
-	part1(m, 2_000_000)
-	start := time.Now()
-	part2(m, 4_000_000)
-	log.Println("part 2 took", time.Since(start))
+	ctx := context.Background()
+	part1(ctx, m, 2_000_000)
+	part2(ctx, m, 4_000_000)
 }
 
-func part1(m Map, y int) {
-	sensors := m.NearbySensorsForRow(y)
-	beacons := m.BeaconsAtRow(y)
+func part1(ctx context.Context, m Map, y int) {
+	min := Pt{math.MinInt, y}
+	max := Pt{math.MaxInt, y}
 
-	beaconMap := aocutil.NewSet[int](len(beacons))
-	for _, pt := range beacons {
-		beaconMap[pt.At.X] = struct{}{}
-	}
-
-	minX := int(math.MaxInt64)
-	maxX := int(math.MinInt64)
-
-	// We need to find the minumum and maximum X that our sensors cover from the
-	// wanted y.
-	for _, sensor := range sensors {
-		minX = aocutil.Min2(minX, sensor.At.X-sensor.Data.BeaconDistance)
-		maxX = aocutil.Max2(maxX, sensor.At.X+sensor.Data.BeaconDistance)
-	}
-
-	coverage := aocutil.NewSet[int](int(maxX - minX))
-	for x := minX; x <= maxX; x++ {
-		if beaconMap.Has(x) {
-			continue
+	var count int64
+	sensorRanges(ctx, m, min, max, func(rs []Range) {
+		for _, r := range rs {
+			atomic.AddInt64(&count, int64(r.To.X-r.From.X+1))
 		}
+	})
 
-		for _, sensor := range sensors {
-			if sensorCovers(sensor, Pt{x, y}) {
-				coverage[x] = struct{}{}
-				break
-			}
-		}
-	}
-
-	var count int
-	for range coverage {
-		count++
-	}
 	fmt.Println("part 1:", count)
 }
 
-// sensorCovers returns true if the given point is within the region that covers
-// from the sensor to the nearest beacon.
-func sensorCovers(sensor MapPt[Sensor], pt Pt) bool {
-	d1 := mdist(sensor.At, pt)
-	d2 := sensor.Data.BeaconDistance
-	return d1 <= d2
-}
-
-func part2(m Map, max int) {
-	type coverage struct{ From, To int }
-
-	sensors := make([]MapPt[Sensor], 0, len(m))
-	for pt, data := range m {
-		if sensor, ok := data.(Sensor); ok {
-			sensors = append(sensors, MapPt[Sensor]{pt, sensor})
-		}
-	}
-
-	// Sort so that the sensors with the largest beacon distance are first.
-	sort.Slice(sensors, func(i, j int) bool {
-		return sensors[i].Data.BeaconDistance < sensors[j].Data.BeaconDistance
-	})
-
-	ctx, cancel := context.WithCancel(context.Background())
+func part2(ctx context.Context, m Map, max int) {
+	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	inputCh := make(chan coverage) // y
-	outputCh := make(chan Pt)
+	distressPtCh := make(chan Pt, 1)
 
-	go func() {
-		defer cancel()
+	sensorRanges(ctx, m, Pt{0, 0}, Pt{max, max}, func(rs []Range) {
+		// Find any gaps within our merged ranges.
+		for i, curr := range rs[1:] {
+			// If the previous range ends before the current range
+			// starts, then we have a gap.
+			if prev := rs[i]; prev.To.X+1 < curr.From.X {
+				distressPtCh <- Pt{prev.To.X + 1, prev.To.Y}
+				cancel()
+				return
+			}
+		}
+	})
 
-		var ranges []coverage
-		var merged []coverage
+	select {
+	case distressPt := <-distressPtCh:
+		fmt.Println("part 2:", tuningFreq(distressPt), distressPt)
+	default:
+		fmt.Println("part 2: no distress signal found")
+	}
+}
+
+func sensorRanges(ctx context.Context, m Map, min, max Pt, fn func([]Range)) {
+	var wg sync.WaitGroup
+	defer wg.Wait()
+
+	in := make(chan [2]int)
+	defer close(in)
+
+	sensors := m.Sensors()
+	beacons := m.Beacons()
+
+	ncpu := aocutil.Min2(runtime.NumCPU(), max.Y-min.Y+1)
+	for i := 0; i < ncpu; i++ {
+		var ranges []Range
+		var merged []Range
 
 		do := func(y int) {
 			// Zero out our variables.
 			ranges = ranges[:0]
 			merged = merged[:0]
 
-			// Precalculate the coverage for this row.
+			// Precalculate the Range for this row.
 			for _, sensor := range sensors {
-				// We can use a trick here: we can calculate how much
-				// coverage a single sensor has for a single row by
-				// subtracting its maximum distance to our Y, both of which
-				// are already calculated.
+				// We can use a trick here: we can calculate how much Range a
+				// single sensor has for a single row by subtracting its maximum
+				// distance to our Y, both of which are already calculated.
 				yDist := abs(y - sensor.At.Y)
 				if sensor.Data.BeaconDistance < yDist {
 					continue
 				}
 
 				xDist := sensor.Data.BeaconDistance - yDist
-				x1 := aocutil.Max2(sensor.At.X-xDist, 0)
-				x2 := aocutil.Min2(sensor.At.X+xDist, max)
-				ranges = append(ranges, coverage{x1, x2})
+				ranges = append(ranges, Range{
+					Pt{aocutil.Max2(sensor.At.X-xDist, min.X), y},
+					Pt{aocutil.Min2(sensor.At.X+xDist, max.X), y},
+				})
 			}
 
 			// Account for beacons too.
-			for _, pt := range m.BeaconsAtRow(y) {
-				if 0 <= pt.At.X && pt.At.X <= max {
-					ranges = append(ranges, coverage{pt.At.X, pt.At.X})
+			for _, beacon := range beacons {
+				if beacon.At.Y == y && min.X <= beacon.At.X && beacon.At.X <= max.X {
+					ranges = append(ranges, Range{beacon.At, beacon.At})
 				}
 			}
 
 			// Sort the ranges in the order that they start so that we can
 			// merge them.
 			sort.Slice(ranges, func(i, j int) bool {
-				return ranges[i].From < ranges[j].From
+				return ranges[i].From.X < ranges[j].From.X
 			})
 
 			// Merge the ranges.
@@ -152,66 +132,54 @@ func part2(m Map, max int) {
 				last := &merged[len(merged)-1]
 				// If the current range starts before the previous range
 				// ends, then we need to merge them.
-				if r.From <= last.To {
+				if r.From.X <= last.To.X {
 					// Extend the previous range to the end of the
 					// current range.
-					last.To = aocutil.Max2(last.To, r.To)
+					last.To.X = aocutil.Max2(last.To.X, r.To.X)
 				} else {
 					// Otherwise, we can just append the current range.
 					merged = append(merged, r)
 				}
 			}
 
-			// Find any gaps within our merged ranges.
-			prev := merged[0]
-			for _, curr := range merged[1:] {
-				// If the previous range ends before the current range
-				// starts, then we have a gap.
-				if prev.To+1 < curr.From {
-					// We'll just assume that this gap is what we want
-					// and is the size of 1.
-					select {
-					case <-ctx.Done():
-					case outputCh <- Pt{prev.To + 1, y}:
-					}
+			fn(merged)
+		}
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case <-ctx.Done():
 					return
-				}
-			}
-		}
-
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case yr := <-inputCh:
-				for y := yr.From; y <= yr.To; y++ {
-					select {
-					case <-ctx.Done():
+				case yrange, ok := <-in:
+					if !ok {
 						return
-					default:
-						do(y)
+					}
+					for y := yrange[0]; y <= yrange[1]; y++ {
+						select {
+						case <-ctx.Done():
+							return
+						default:
+							do(y)
+						}
 					}
 				}
 			}
-		}
-	}()
+		}()
+	}
 
-	var distressPos Pt
+	const batch = 10_000
+	for y1 := min.Y; y1 <= max.Y; y1 += batch {
+		y2 := aocutil.Min2(y1+batch, max.Y)
 
-	batch := max / runtime.NumCPU()
-search:
-	for y := int(0); y < max; y += batch {
 		select {
 		case <-ctx.Done():
-			break search
-		case distressPos = <-outputCh:
-			break search
-		case inputCh <- coverage{y, aocutil.Min2(y+batch, max)}:
+			return
+		case in <- [2]int{y1, y2}:
 			// ok
 		}
 	}
-
-	fmt.Println("part 2:", tuningFreq(distressPos), distressPos)
 }
 
 const distressC = 4_000_000
@@ -220,50 +188,40 @@ func tuningFreq(pt Pt) int {
 	return (pt.X * distressC) + pt.Y
 }
 
-type Pt struct{ X, Y int }
+// Pt is a point in 2D space.
+type Pt = image.Point
 
+// Range is a range of points.
+type Range struct {
+	From, To Pt
+}
+
+// Map is a map of the area.
 type Map map[Pt]Data
 
+// MapPt is a point in 2D space with data.
 type MapPt[T Data] struct {
 	At   Pt
 	Data T
 }
 
-func (m Map) DrawMap(w io.Writer, start, end Pt) {
-	for y := start.Y; y <= end.Y; y++ {
-		for x := start.X; x <= end.X; x++ {
-			p := Pt{x, y}
-			if data, ok := m[p]; ok {
-				w.Write([]byte{data.Data()})
-			} else {
-				w.Write([]byte("."))
-			}
-		}
-		w.Write([]byte{'\n'})
-	}
-}
-
-// NearbySensorsForRow returns the list of coordinates of the sensors that are
-// near the given row. A sensor is near if its signal reaches the given row.
-func (m Map) NearbySensorsForRow(y int) []MapPt[Sensor] {
-	var pts []MapPt[Sensor]
+// Beacons returns the list of sensors.
+func (m Map) Beacons() []MapPt[Beacon] {
+	var pts []MapPt[Beacon]
 	for pt, data := range m {
-		if sensor, ok := data.(Sensor); ok {
-			if pt.Y-sensor.BeaconDistance <= y && y <= pt.Y+sensor.BeaconDistance {
-				pts = append(pts, MapPt[Sensor]{pt, sensor})
-			}
+		if beaecon, ok := data.(Beacon); ok {
+			pts = append(pts, MapPt[Beacon]{pt, beaecon})
 		}
 	}
 	return pts
 }
 
-// BeaconsAtRow returns the list of coordinates of the beacons that are at the
-// given row.
-func (m Map) BeaconsAtRow(y int) []MapPt[Beacon] {
-	var pts []MapPt[Beacon]
+// Sensors returns the list of sensors.
+func (m Map) Sensors() []MapPt[Sensor] {
+	var pts []MapPt[Sensor]
 	for pt, data := range m {
-		if beacon, ok := data.(Beacon); ok && pt.Y == y {
-			pts = append(pts, MapPt[Beacon]{pt, beacon})
+		if sensor, ok := data.(Sensor); ok {
+			pts = append(pts, MapPt[Sensor]{pt, sensor})
 		}
 	}
 	return pts

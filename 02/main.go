b66076bc5067e1047d0a5f75dad740219224bb9e
diamondburned <diamond@arikawa-hi.me>
2023-12-01 21:39:45 -0800

    Clean up 02

--- 02/main.go
+++ 02/main.go
@@ -32,22 +32,24 @@ var gameLine = regexp.MustCompile(`Game \d*: (.*;? *)`)
 func parseGames(input string) []Game {
 	lines := aocutil.SplitLines(input)
 	games := make([]Game, 0, len(lines))
+
 	for _, line := range lines {
 		match := gameLine.FindStringSubmatch(line)
 		if match == nil {
 			continue
 		}
 
-		parts := strings.Split(match[1], ";")
+		parts := strings.Split(match[1], "; ")
 		var game Game
+
 		for _, part := range parts {
-			part = strings.TrimSpace(part)
-			balls := strings.Split(part, ",")
+			balls := strings.Split(part, ", ")
 			var pull Pull
+
 			for _, ball := range balls {
-				ball = strings.TrimSpace(ball)
 				value, name, _ := strings.Cut(ball, " ")
 				valueInt, _ := strconv.Atoi(value)
+
 				switch name {
 				case "blue":
 					pull.Blue = valueInt
@@ -59,21 +61,23 @@ func parseGames(input string) []Game {
 					log.Panicf("unknown ball color %q", name)
 				}
 			}
+
 			game.Pulls = append(game.Pulls, pull)
 		}
+
 		games = append(games, game)
 	}
+
 	return games
 }
 
 func part1(input string) {
 	games := parseGames(input)
 
-	wantRed := 12
-	wantGreen := 13
-	wantBlue := 14
+	const wantRed = 12
+	const wantGreen = 13
+	const wantBlue = 14
 
-	possibleGames := make([]Game, 0, len(games))
 	var idSum int
 searchLoop:
 	for i, game := range games {
@@ -82,7 +86,6 @@ searchLoop:
 				continue searchLoop
 			}
 		}
-		possibleGames = append(possibleGames, game)
 		idSum += i + 1
 	}
 
@@ -91,45 +94,16 @@ searchLoop:
 
 func part2(input string) {
 	games := parseGames(input)
-	//
-	// wantRed := 12
-	// wantGreen := 13
-	// wantBlue := 14
-
-	// 	possibleGames := make([]Game, 0, len(games))
-	// searchLoop:
-	// 	for _, game := range games {
-	// 		for _, pull := range game.Pulls {
-	// 			if pull.Blue > wantBlue || pull.Red > wantRed || pull.Green > wantGreen {
-	// 				continue searchLoop
-	// 			}
-	// 		}
-	// 		possibleGames = append(possibleGames, game)
-	// 	}
-
-	type maxGame struct {
-		MaxBlue  int
-		MaxRed   int
-		MaxGreen int
-		Power    int
-	}
 	var powerSum int
-	for i, game := range games {
-		var maxGame maxGame
-		for _, pull := range game.Pulls {
-			if pull.Blue > maxGame.MaxBlue {
-				maxGame.MaxBlue = pull.Blue
-			}
-			if pull.Red > maxGame.MaxRed {
-				maxGame.MaxRed = pull.Red
-			}
-			if pull.Green > maxGame.MaxGreen {
-				maxGame.MaxGreen = pull.Green
-			}
-		}
-		maxGame.Power = maxGame.MaxBlue * maxGame.MaxRed * maxGame.MaxGreen
-		log.Printf("Game %d: power = %d", i+1, maxGame.Power)
-		powerSum += maxGame.Power
+	for _, game := range games {
+		maxBlue := aocutil.Maxs(
+			aocutil.Transform(game.Pulls, func(p Pull) int { return p.Blue })...)
+		maxRed := aocutil.Maxs(
+			aocutil.Transform(game.Pulls, func(p Pull) int { return p.Red })...)
+		maxGreen := aocutil.Maxs(
+			aocutil.Transform(game.Pulls, func(p Pull) int { return p.Green })...)
+		power := maxBlue * maxRed * maxGreen
+		powerSum += power
 	}
 
 	fmt.Println(powerSum)

1aff34cfdf8554233b2e3fbcb114be3be953a055
diamondburned <diamond@arikawa-hi.me>
2023-12-15 20:57:07 -0800

    Add back template.go

---
+++ template.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/diamondburned/aoc-2022/aocutil"
+
+func main() {
+	aocutil.Run(part1, part2)
+}
+
+type T struct {
+}
+
+func parseInput(input string) T {
+	return T{}
+}
+
+func part1(input string) int {
+	return 0
+}
+
+func part2(input string) int {
+	return 0
+}
+
